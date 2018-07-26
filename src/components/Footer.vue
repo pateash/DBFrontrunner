cfe6c421a5a32e7429946c1c3ba53b3914abe976
Ashish Patel <ashishpatel0720@gmail.com>
2018-07-27 00:41:36 +0530

    FOOTER: added

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,22 @@
 <template>
-    
+    <section class="hero"  style="margin-top: 5em;">
+  <div class="hero-body" style="padding-bottom: 1em">
+    <div class="container">
+      <h2 class="subtitle">
+      Copyright&copy;2018. <b>Deutsche Bank AG</b>. All rights reserved.
+      </h2>
+    </div>
+  </div>
+</section>
 </template>
 
 <script>
+    import Hero from "./Hero";
     export default {
-        name: "Footer"
+        name: "Footer",
+        components:{
+            Hero
+        }
     }
 </script>
 
